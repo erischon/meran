5c6a4ca7bd963600723e3a645faf07ea03d51479
erischon <66017574+erischon@users.noreply.github.com>
2023-11-02 15:54:02 +0100

    misc

---
+++ src/app/(root)/(routes)/expenses/page.tsx
@@ -0,0 +1,11 @@
+import ExpenseForm from "@/components/expenses/ExpenseForm";
+
+export default function ExpensePage() {
+  return (
+    <main>
+      <h1>ExpensePage</h1>
+
+      <ExpenseForm />
+    </main>
+  );
+}
---
+++ src/app/(root)/(routes)/layout.tsx
@@ -0,0 +1,13 @@
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full relative">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-stone-900">
+        The Sidebar
+      </div>
+    </div>
+  );
+}
